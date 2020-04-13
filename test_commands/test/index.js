f0f57cd6cc5b1a6163b2d38fa75111efd4ad1af4
JaimeRC <jaime.rc@icloud.com>
2020-04-13 19:49:55 +0200

    Update tests

--- test_commands/test/index.js
+++ test_commands/test/index.js
@@ -1,11 +1,20 @@
+const Redis = require('ioredis')
+
 describe('RedisDB Testing', () => {
 
     global.OPTS_REDIS = {host: 'redis'}
+    beforeEach(() => (new Redis(global.OPTS_REDIS)).flushall())
 
    // require('./connection.test')
 
-    //require('./commands.test')
+   // require('./commands.test')
 
-    require('./command.hash.test')
+  //  require('./command.hash.test')
+
+   // require('./commands.incr.decr.test')
+
+    //require('./commands.sets.test')
+
+    require('./petitions.limit.test')
 
 })
